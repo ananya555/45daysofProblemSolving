d6bb62bd01da854cca13b5ec9500aa97b6fb0962
Ananya Goel <88566978+ananya555@users.noreply.github.com>
2022-05-16 18:19:53 +0530

    Create D7Sort Colors.cpp

---
+++ D7Sort Colors.cpp
@@ -0,0 +1,22 @@
+// Problem Link
+//https://leetcode.com/problems/sort-colors/
+
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        
+        // DUTCH FLAG Approach
+        
+        int low = 0, mid = 0, high = arr.size()-1;
+        
+        while(mid <= high)
+        {
+            if(arr[mid] == 0)
+                swap(arr[low++], arr[mid++]);
+            else if(arr[mid] == 1)
+                mid++;
+            else
+                swap(arr[mid], arr[high--]);
+        }
+    }
+};
